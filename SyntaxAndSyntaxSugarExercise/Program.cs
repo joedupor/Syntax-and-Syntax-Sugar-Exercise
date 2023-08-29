860b4e39a86da29f6eb1e1c6a12bddeb90f69546
joedupor <joedupor@gmail.com>
2023-08-29 15:00:51 -0500

    initial commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = "";
+
+            var result = (answer < 9) ? response = $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(result);
+
+            //original code to convert:
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "is greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+            //an alternate solution
+            //var answer = 4;
+            //var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            //Console.WriteLine(response);
+
+
         }
     }
 }
